0b7641022e2e39b47bafc1d7e5ad5744cd83a767
maksym <1maksymrusanov1@gmail.com>
2025-11-24 19:44:50 +0000

    add agent 3

---
+++ exercises/ReAct.py
@@ -0,0 +1,85 @@
+from typing import Annotated, Sequence, TypedDict
+
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, SystemMessage
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END, StateGraph, message
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+
+load_dotenv()
+
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+@tool
+def add(a: int, b: int):
+    """
+    this is add func that add 2 numbers
+    """
+    return a + b
+
+
+@tool
+def subtract(a, b):
+    """
+    subtraction function
+    """
+    return a - b
+
+
+@tool
+def multiply(a, b):
+    """
+    multiply function
+    """
+    return a * b
+
+
+tools = [add]
+model = ChatOpenAI(model="gpt-4o").bind_tools(tools)
+
+
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(
+        content="you are my AI assistant,please answer my query to the best of your ability "
+    )
+    response = model.invoke([system_prompt] + state["messages"])
+    return {"messages": [response]}
+
+
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+tool_node = ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges(
+    "our_agent", should_continue, {"continue": "tools", "end": END}
+)
+graph.add_edge("tools", "our_agent")
+app = graph.compile()
+
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+inputs = {"messages": ["user", "add 3+5"]}
+print_stream(app.stream(inputs, stream_mode="values"))
